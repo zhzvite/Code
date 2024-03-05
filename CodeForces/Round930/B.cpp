2f0cdb315ee8a9131d6f69afb14e1be1f3381d1b
zhz_vite <2811215248@qq.com>
2024-03-05 08:17:35 +0800

    Update B.cpp

--- CodeForces/Round930/B.cpp
+++ CodeForces/Round930/B.cpp
@@ -17,46 +17,35 @@ const int N = 2e5 + 10;
 int n, t;
 char a1[N], a2[N];
 char a[N];
-int cnt ;
-int res ;
-void find(int x,int y){
-    if(x==1){
-        if((a1[y+1]=='0'||(a1[y+1]=='1'&&a2[y]=='1'))&& y+1<=n){
-            a[cnt++] = a1[y + 1];
-            if(a1[y+1]==a2[y])
-                res++;
-            else
-                res = 1;
-            find(x, y + 1);
-        }
-        else if(y+1>n)
-            {a[cnt++] = a2[y];
-                return;
-            }
-        else {
-            for (int i = y; i <= n;i++)
-                a[y + 1] = a2[y];
-            return;
-        }
-    }
-}
 int main(){
     cin >> t;
     while(t--){
         cin >> n;
-        res = 1;
-        cnt = 2;
         for (int i = 1; i <=n;i++)
             cin >> a1[i];
         for (int i = 1; i <= n;i++)
             cin >> a2[i];
-        a[1] = a1[1];
-        find(1, 1);
-        for (int i = 1; i <= n+1 ;i++)
-            cout << a[i];
-        cout << a2[n];
-        cout << endl;
-        //cout << cnt << endl;
-        cout << res << endl;
+        int c = 0, i, l = 0, r = 0;
+        for (i = 1; i < n;i++){
+            if(a1[i+1]==a2[i])
+                c++, r++;
+            else if(a1[i+1]>a2[i])
+                break;
+            else
+                c = 0, r = l = i + 1;
+        }
+        if(i<=n){
+            for (int j = 1; j <= i;j++)
+                printf("%c", a1[j]);
+            for (int j = i; j <= n; j++)
+                printf("%c", a2[j]);
+        }
+        else
+        {
+            printf("%s", a1+ 1);
+            printf("%c", a2[n]);
+        }
+        printf("\n%d\n", c + 1);
+        
     }
 }
